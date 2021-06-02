86ed5ee398924fdc284705179b42b0735eb7b3d2
HungPhamdev <85250994+HungPhamdev@users.noreply.github.com>
2021-06-03 01:05:07 +0700

    File Javascript

---
+++ Chinhanh.js
@@ -0,0 +1,16 @@
+[{
+    "Id": "hcm",
+    "Name": "Hồ Chí Minh"
+}, {
+    "Id": "dn",
+    "Name": "Đà Nẵng"
+}, {
+    "Id": "tn",
+    "Name": "Tây Nguyên"
+}, {
+    "Id": "ct",
+    "Name": "Cần Thơ"
+}, {
+    "Id": "hn",
+    "Name": "Hà Nội"
+}]
---
+++ Students.js
@@ -0,0 +1,41 @@
+﻿var students = [{
+        "fullname": "Phạm Lý Hùng",
+        "password": "hungdz",
+        "phone": 0937643881,
+        "email": "hungplps13663@fpt.edu.vn",
+        "gender": "true",
+        "schoolfee": 1500000,
+        "subject": "ASNE",
+        "cnhanh": "hcm"
+    },
+    {
+        "fullname": "Nguyễn Văn Tèo",
+        "password": "iloveyou",
+        "phone": 0945453347,
+        "email": "teonv@fpt.edu.vn",
+        "gender": "true",
+        "schoolfee": 1500000,
+        "subject": "CLCO",
+        "cnhanh": "hn"
+    },
+    {
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "password": "iloveyou",
+        "phone": 0945453347,
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "schoolfee": 1500000,
+        "subject": "DBBS",
+        "cnhanh": "ct"
+    },
+    {
+        "fullname": "Phạm Thị Nở",
+        "password": "iloveyou",
+        "phone": 0945453347,
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "schoolfee": 2000000,
+        "subject": "GAME",
+        "cnhanh": "dn"
+    }
+]
